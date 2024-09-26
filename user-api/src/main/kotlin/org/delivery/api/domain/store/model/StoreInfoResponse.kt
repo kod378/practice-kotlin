d0845fe8cb370aeb89405fecbc25ceca5fedab9b
kod378 <kod378@naver.com>
2024-09-27 01:42:46 +0900

    스토어 출력 부분 수정

---
+++ user-api/src/main/kotlin/org/delivery/api/domain/store/model/StoreInfoResponse.kt
@@ -0,0 +1,18 @@
+package org.delivery.api.domain.store.model
+
+import org.delivery.api.domain.storemenu.model.StoreMenuResponse
+import org.delivery.db.store.enums.StoreCategory
+import java.math.BigDecimal
+
+data class StoreInfoResponse(
+    val id: Long,
+    val name: String,
+    val address: String,
+    val category: StoreCategory,
+    val star: Double,
+    val thumbnailUrl: String,
+    val minimumAmount: BigDecimal,
+    val minimumDeliveryAmount: BigDecimal,
+    val phoneNumber: String,
+    val menus: List<StoreMenuResponse>
+)
