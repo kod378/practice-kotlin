427fa4605cc45b83f0c57cb907e42a578b3e34e6
kod378 <kod378@naver.com>
2024-10-02 22:42:52 +0900

    1. rabbitmq 처음 exchage랑 queue 만드는 헬스체크 다시 추가

--- user-api/src/main/kotlin/org/delivery/api/config/health/HealthOpenApiController.kt
+++ user-api/src/main/kotlin/org/delivery/api/config/health/HealthOpenApiController.kt
@@ -1,5 +1,6 @@
 package org.delivery.api.config.health
 
+import org.delivery.api.common.rabbitmq.Producer
 import org.delivery.common.log.logger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -7,12 +8,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/open-api")
-class HealthOpenApiController {
+class HealthOpenApiController(
+    private val producer: Producer
+) {
 
     val log = logger()
 
     @GetMapping("/health")
     fun health() {
         log.info("health call")
+        producer.producer("delivery.exchange", "delivery.key", "health call")
     }
 }
